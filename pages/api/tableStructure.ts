ee5c5e5eb59a43710803d11aa3370eba78fd22ca
Rachel-Taskale <racheltaskale@gmail.com>
2025-06-13 19:43:38 -0400

    scaffolding

---
+++ pages/api/tableStructure.ts
@@ -0,0 +1,29 @@
+// pages/api/nodes.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma";
+
+async function get(req: NextApiRequest, res: NextApiResponse) {
+  const nodes = await prisma.artist.findMany();
+  return res.status(200).json(nodes);
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    switch (req.method) {
+      case "GET":
+        return await get(req, res);
+
+      default:
+        return res.setHeader("Allow", ["GET", "POST"]).status(405).end();
+    }
+  } catch (error: any) {
+    console.error("API Error:", error);
+    return res.status(500).json({
+      error: "Internal Server Error",
+      details: error.message,
+    });
+  }
+}
